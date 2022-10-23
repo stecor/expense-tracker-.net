4cce5c3878e05ebdbc92c47a418435e95331ba5c
Stefano Corra <stefano.corra@hotmail.com>
2022-10-23 17:13:47 -0400

    category commit

--- Expense Tracker/Models/Category.cs
+++ Expense Tracker/Models/Category.cs
@@ -13,5 +13,14 @@ namespace Expense_Tracker.Models
         public string Icon { get; set; } = "";
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
+
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
     }
 }
